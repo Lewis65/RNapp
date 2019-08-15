0f4ec885ebbceac758a434377395b72cb52d7383
Lewis Horwood <horwood.lewis@gmail.com>
2019-08-15 17:26:45 +0900

    create reducer

---
+++ reducers/count.tsx
@@ -0,0 +1,16 @@
+import { combineReducers} from 'redux'
+
+const initialState = {
+    count: 0
+}
+
+const countReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default combineReducers({
+    count: countReducer
+})
